19d740ad30c5925abd5a9e3d779236f01fe21a55
Julien Daoust <juliendaoust@MacBook-Pro-de-Julien.local>
2017-04-11 22:31:42 -0400

    Game commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("LoadLevel request: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log("QuitLevelRequest: ");
+		Application.Quit();
+	}
+
+}
